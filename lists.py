71b53b8fad6f0fd102c0a3e2bd45e708064f97ad
lewandowskimariusz <mariusz.k.lewandowskI@gmail.com>
2024-05-21 11:52:29 +0200

    LinkedLists

--- lists.py
+++ lists.py
@@ -1,1 +1,20 @@
-hey
+class Node:
+    def __init__(self, value):
+        self.value = value
+
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.head = new_node
+        self.length = 1 
+    def append(self, value):
+        pass
+    def prepend(self, value):
+        pass
+    def insert(self, index, value):
+        pass
+
+my_linked_list = LinkedList(4)
+print(my_linked_list.head.value)
